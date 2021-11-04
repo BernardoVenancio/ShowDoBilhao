6a18089b19a542e20d14bf14c4158b6667910894
BernardoVenancio <bernardovenancio1@gmail.com>
2021-11-04 18:28:37 -0300

    Alguns ajustes e adição da tela de vitória (muito boa diga-se de passagem)

--- verificaResposta.php
+++ verificaResposta.php
@@ -2,9 +2,13 @@
     include "perguntas.inc";
 
     $id = $_POST['id'] + 1;
-    if($verificaResposta($_POST['id'])){
-        header ("location: http://localhost/ShowDoBilhao/perguntinha.php?id=$id"); 
-    } else header ("location: http://localhost/ShowDoBilhao/lose.html"); 
+    $pontuaçãoMaxima = $getPontuacaoMaxima();
     
-
+    if ($verificaResposta($_POST['id']) && $id < $pontuaçãoMaxima){
+        header ("location: http://localhost/ShowDoBilhao/perguntinha.php?id=$id"); 
+    } else if ($verificaResposta($_POST['id'])){
+        header ("location: http://localhost/ShowDoBilhao/vitoria.html"); 
+    } else header ("location: http://localhost/ShowDoBilhao/derrota.html"); 
+     
+    
 ?>
